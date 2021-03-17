fb4e18f565a88e5f0b6eee509208c8fe98807e8d
mohanjnv1 <mohanjnv1@gmail.com>
2021-03-17 21:13:43 +0530

    changes by mohan

---
+++ client/codingcable/src/components/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+// import reportWebVitals from "../../reportWebVitals";
+// import { BrowserRouter as Router } from "react-router-dom";
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
--- client/codingcable/src/index.js
+++ client/codingcable/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
