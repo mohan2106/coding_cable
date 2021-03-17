d1cd2a73b3e0ce25dfb52f0b3696e0efc68efcbe
aman <amankrsingh1812@gmail.com>
2021-03-17 13:32:46 +0530

    Frontend Build 1

--- server/routes/login.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { loginController } from '../controllers/loginController.js';
-import { createToken } from '../middlewares/verifyToken.js'
-
-const router = express.Router();
-
-router.post('/', loginController, createToken);
-
-export default router;
