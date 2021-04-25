2a59fcef4829eee0cd4bc6f599cbab7d9f168243
Mohan Kumar <mohanjnv1@gmail.com>
2021-04-25 16:23:08 +0530

    dashboard is added

--- client/codingcable/src/App.js
+++ client/codingcable/src/App.js
@@ -6,6 +6,7 @@ import Login from './components/LoginPage/LoginPage';
 import CourseDetail from './components/CourseDetailPage/CourseDetail';
 import FAQPage from './components/FAQPage/FAQPage';
 import AboutUsPage from './components/AboutUsPage/AboutUs';
+import Dashboard from './components/Dashboard/Dashboard';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path='/courses/:id' component={CourseDetail}/>
         <Route path='/faqs' exact component={FAQPage}/>
         <Route path='/aboutus' exact component={AboutUsPage}/>
+        <Route path='/dashboard/:id' exact component={Dashboard}/>
       </Switch>
     </Router>
   );
