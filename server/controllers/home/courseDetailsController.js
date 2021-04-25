217da1f548acd22c7423e247df81b5e989cce329
aman <amankrsingh1812@gmail.com>
2021-04-25 15:07:37 +0530

    Minor Fix

--- server/controllers/home/courseDetailsController.js
+++ server/controllers/home/courseDetailsController.js
@@ -5,7 +5,7 @@ export const courseDetails = (req, res, next) => {
   const collection = CC_DB_OBJ.collection("CourseDetails");
   var courseID = req.query["id"];
   log.info(req.query);
-  collection.findOne({ Id: courseID }, function (err, docs) {
+  collection.findOne({ CourseId: courseID }, function (err, docs) {
     if (err) return res.status(500).send({ message: "Internal Server Error" });
 
     return res.status(200).send(docs);
