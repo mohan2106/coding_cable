941d4246da1686a48dc4b2e90a64817018cc265e
aman <amankrsingh1812@gmail.com>
2021-05-01 22:23:41 +0530

    Bugs Fixed

--- server/controllers/blogs/getBlogController.js
+++ server/controllers/blogs/getBlogController.js
@@ -1,21 +1,12 @@
 import { log } from "../../index.js";
 import { CC_DB_OBJ } from "../../index.js";
+import mongo from "mongodb";
 
 export const getBlog = (req, res, next) => {
+  let id = new mongo.ObjectID(req.body.id);
   const collection = CC_DB_OBJ.collection("Blogs");
   collection
-    .findOne({ _id: req.body.id })
-    .project(
-      {
-        UserName: 1,
-        UserImage: 1,
-        TitleImage: 1,
-        Title: 1,
-        Date: 1,
-        Content: 1,
-        Description: 1,
-      },
-      function (err, docs) {
+    .findOne({ _id: id  },function (err, docs) {
         log.info(docs);
         if (!docs) return res.status(404).send({ message: "blog not found" });
 
@@ -23,3 +14,13 @@ export const getBlog = (req, res, next) => {
       }
     );
 };
+// .project(
+//   {
+//     UserName: 1,
+//     UserImage: 1,
+//     TitleImage: 1,
+//     Title: 1,
+//     Date: 1,
+//     Content: 1,
+//     Description: 1,
+//   },
