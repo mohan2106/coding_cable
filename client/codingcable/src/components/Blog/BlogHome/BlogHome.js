1cf4acc4cc2a6abf05996a5066d8b25cd72f35ce
Mohan Kumar <mohanjnv1@gmail.com>
2021-05-02 01:35:07 +0530

    blogs api added

--- client/codingcable/src/components/Blog/BlogHome/BlogHome.js
+++ client/codingcable/src/components/Blog/BlogHome/BlogHome.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import classes from './BlogHome.module.css';
+import loadingclass from './loading.module.css';
 import {Link} from 'react-router-dom';
+import ErrorPage from '../../ErrorPage/ErrorPage'; 
+import axios from 'axios';
 
 
 const Single = (props) => {
+    // const timestamp = props.TimeStamp;
+    const date = new Date(props.TimeStamp);
+    const stringdate = date.toDateString();
     return (
         <div className={classes.single}>
             <div className={classes.singlecontainer}>
@@ -11,7 +17,7 @@ const Single = (props) => {
                     <div className={classes.username}>Written By: {props.UserName}</div>
                     <div className={classes.title}>{props.Title}</div>
                     <div className={classes.desc}>{props.Description}</div>
-                    <div className={classes.date}>{props.Date}</div>
+                    <div className={classes.date}>{stringdate}</div>
                 </div>
                 <div className={classes.item2}>
                     <img src={props.TitleImageLink} alt="img"/>
@@ -32,49 +38,104 @@ const BlogBanner = ()=>{
     );
 }
 
-const data = [
-    {
-		ID:'ID1',
-        Title: 'Sample Blog 1',
-        TitleImageLink: 'https://images.unsplash.com/photo-1619314367068-f1ba7b7ebfaf?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-        Description: 'This is a sample description, Once apon a time in mumbai',
-        UserName: 'Mohan Kumar',
-        UserPic: 'NOthing',
-        Date: '25th Dec 1998'
-    },
-    {
-		ID:'ID2',
-        Title: 'Sample Blog 2',
-        TitleImageLink: 'https://images.unsplash.com/photo-1619478691745-af923eed4601?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-        Description: 'This is a sample description',
-        UserName: 'Mohan Kumar',
-        UserPic: 'NOthing',
-        Date: '25th Dec 1998'
-    },
-    {
-		ID:'ID1',
-        Title: 'Sample Blog 3',
-        TitleImageLink: 'https://images.unsplash.com/photo-1619314367068-f1ba7b7ebfaf?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-        Description: 'This is a sample description',
-        UserName: 'Mohan Kumar',
-        UserPic: 'NOthing',
-        Date: '25th Dec 1998'
-    },
-
-]
-
-function BlogHome() {
-    const dataList = data.map(d => {
-        const link = "/blogs/id/"+d.ID;
-        return (<Link to={link} style={{ textDecoration: 'none' }}>
-            <Single {...d}/>
-        </Link>);
+const SingleLoading = () => {
+    return (
+        <div className={loadingclass.single}>
+            <div className={loadingclass.item1}>
+                <div className={loadingclass.name}></div>
+                <div className={loadingclass.head}></div>
+                <div className={loadingclass.subhead}></div>
+            </div>
+            <div className={loadingclass.item2}></div>
+        </div>
+    );
+}
+const Loading = () => {
+    const data = [0,1,2,3,4,5,6,7,8,9].map(i=>{
+        return <SingleLoading/>;
     })
+    return (
+        <div className={loadingclass.loading}>
+            {data}
+        </div>
+    );
+}
+
+
+function BlogHome(props) {
+
+    const [index,setIndex] = useState(0);
+    const [loading,setLoading] = useState(false);
+    const [blogs,setBlogs] = useState({});
+    const [error,setError] = useState('');
+    const [blogComponent,setBlogComponent] = useState(null);
+    // console.log('Props called',props.blogData);
+    // if(blogData === ''){
+    //     
+    // }
+    // setBlog(props.blogData);
+    
+    const blogFetch = () => {
+        setLoading(true);
+        setBlogs({});
+        setError('');
+        setBlogComponent(Loading())
+    }
+    const blogSuccess = (data) => {
+        setLoading(false);
+        setBlogs(data);
+        setError('');
+        const dataList = data.BlogList.map(d => {
+            const link = "/blogs/id/"+d._id;
+            return (<Link to={link} style={{ textDecoration: 'none' }}>
+                <Single {...d}/>
+            </Link>);
+        });
+        // setBlogComponent(Loading())
+        setBlogComponent(dataList);
+    }
+    const blogFailure = (data) => {
+        setLoading(false);
+        setBlogs({});
+        setError(data);
+        
+        setBlogComponent(<ErrorPage message={data}/>);
+    }
+
+    useEffect(() => {
+        var data = JSON.stringify(
+            {
+                "st":10*index,
+                "end":(10*(index+1)-1)
+            }
+        );
+        var config = {
+            method: 'post',
+            url: 'http://localhost:4000/blogs/getRange',
+            headers: { 
+                'Content-Type': 'application/json',
+            },
+            data : data  
+        };
+        blogFetch();
+        axios(config)
+        .then(response => {
+            const blogDetail = response.data;
+            console.log(blogDetail);
+            blogSuccess(blogDetail);
+        }).catch(error => {
+            const errorMsg = error.message;
+            blogFailure(errorMsg);
+            console.log(errorMsg);
+        })
+        
+    }, [])
+    
     return (
         <div className={classes.container}  >
             <BlogBanner/>
             <div className={classes.blogsContainer}></div>
-            {dataList}
+            {blogComponent}
         </div>
     )
 }
--- client/codingcable/src/components/Blog/SingleBlog/SingleBlog.js
+++ client/codingcable/src/components/Blog/SingleBlog/SingleBlog.js
@@ -2,23 +2,23 @@ import classes from "./SingleBlog.module.css";
 import loadingclass from './loading.module.css';
 import React,{useState,useEffect} from "react";
 import Editor from "rich-markdown-editor";
-import {light as Light,dark as Dark} from '../theme.ts';
-import { connect } from "react-redux";
-import {getBlog} from '../../../redux/index';
+import {light as Light} from '../theme.ts';
+// import { connect } from "react-redux";
+// import {getBlog} from '../../../redux/index';
 import axios from 'axios';
 import ErrorPage from '../../ErrorPage/ErrorPage';
 
 
-const data =  {
-    UserName: 'Mohan Kumar',
-    UserImage: 'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    TitleImage: 'https://images.unsplash.com/photo-1619478691745-af923eed4601?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    Title: 'Tim Cook’s 5-Word Response to Facebook Is Brutal and Brilliant at the Same Time',
-    Date: '25th Dec 2020',
-    // Content: 'Hello World2',
-    Content: '---    __Advertisement :)__\n - __[pica](https://nodeca.github.io/pica/demo/)__ - high quality and fast image \n  resize in browser. \n    - __[babelfish](https://github.com/nodeca/babelfish/)__ - developer friendly \n   i18n with plurals support and easy syntax. \n    You will like those projects! \n  --- \n  # h1 Heading 8-) \n ## h2 Heading \n ### h3 Heading  \n   #### h4 Heading \n ##### h5 Heading \n  ###### h6 Heading \n',
-    Description:'Apple’s CEO is clear that he isn’t making decisions based on what’s best for Facebook, but for Apple and its customers',
-}
+// const data =  {
+//     UserName: 'Mohan Kumar',
+//     UserImage: 'https://images.unsplash.com/photo-1542103749-8ef59b94f47e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+//     TitleImage: 'https://images.unsplash.com/photo-1619478691745-af923eed4601?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+//     Title: 'Tim Cook’s 5-Word Response to Facebook Is Brutal and Brilliant at the Same Time',
+//     Date: '25th Dec 2020',
+//     // Content: 'Hello World2',
+//     Content: '---    __Advertisement :)__\n - __[pica](https://nodeca.github.io/pica/demo/)__ - high quality and fast image \n  resize in browser. \n    - __[babelfish](https://github.com/nodeca/babelfish/)__ - developer friendly \n   i18n with plurals support and easy syntax. \n    You will like those projects! \n  --- \n  # h1 Heading 8-) \n ## h2 Heading \n ### h3 Heading  \n   #### h4 Heading \n ##### h5 Heading \n  ###### h6 Heading \n',
+//     Description:'Apple’s CEO is clear that he isn’t making decisions based on what’s best for Facebook, but for Apple and its customers',
+// }
 
 const Loading = () => {
     return (
@@ -58,26 +58,29 @@ const Loading = () => {
 
 function SingleBlog(props) {
     const [loading,setLoading] = useState(false);
-    const [blog,setBlog] = useState({});
+    const [data,setData] = useState({});
     const [error,setError] = useState('');
-    console.log('Props called',props.blogData);
+    // console.log('Props called',props.blogData);
     // if(blogData === ''){
     //     
     // }
     // setBlog(props.blogData);
     const blogFetch = () => {
         setLoading(true);
-        setBlog({});
+        setData({});
         setError('');
     }
     const blogSuccess = (data) => {
         setLoading(false);
-        setBlog(data);
+        const date = new Date(data.TimeStamp);
+        const stringdate = date.toDateString();
+        setData({...data,
+            TimeStamp:stringdate});
         setError('');
     }
     const blogFailure = (data) => {
         setLoading(false);
-        setBlog({});
+        setData({});
         setError(data);
     }
 
@@ -107,7 +110,7 @@ function SingleBlog(props) {
         blogFetch();
         axios(config)
         .then(response => {
-            const blogDetail = response.data;
+            const blogDetail = response.data.blog;
             console.log(blogDetail);
             blogSuccess(blogDetail);
         }).catch(error => {
@@ -121,17 +124,17 @@ function SingleBlog(props) {
         <div className={classes.container}>
             {loading ? <Loading/> : 
             <div>
-                {error.length !== 0 ? <ErrorPage/> : 
+                {error.length !== 0 ? <ErrorPage message="PAGE NOT FOUND"/> : 
                 <div>
                 <div className={classes.title}>{data.Title}</div>
                 <div className={classes.desc}>{data.Description}</div>
                 <div className={classes.user}>
                     <div className={classes.item1}>
-                        <img className={classes.userImage} src={data.UserImage} alt="userImage"/>
+                        <img className={classes.userImage} src={data.UserImage} alt="img"/>
                     </div>
                     <div className={classes.item2}>
                         <div className={classes.username}>{data.UserName}</div>
-                        <div className={classes.date}>{data.Date}</div>
+                        <div className={classes.date}>{data.TimeStamp}</div>
                     </div>
                 </div>
                 <div >
